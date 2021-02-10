4b5ff25313f6fae790675ae299088f0e5c3e4f71
Sebastian Aristondo <ari20880@uvg.edu.gt>
2021-02-10 17:41:13 -0600

    Lectura archivo
    Se genera el archivo con los 3000 numeros a sortear

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-public class Main {
+public class Main
+{
+    public  static  void main(String[] arg) {
+        /**Declaracion de clases*/
+        Randomizador R = new Randomizador();
+        BubbleSort burbuja = new BubbleSort();
+
+        //-----------------------------------------
+
+        R.generate();
+        /**Se recibe lista a ordenar*/
+        Comparable[] ListaAOrdenar= R.lectura();
+
+
+
+
+    }
+
 }
---
+++ src/Randomizador.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+/**Clase que se encarga de generar los numeros, leer y crear el archivo*/
+public class Randomizador {
+
+    Random rnd;
+    public Randomizador()
+    {
+        rnd = new Random();
+    }
+
+    public  void generate()
+    {
+        String acu="";
+        for (int i =0; i< 3000;i++)
+        {
+            acu+=rnd.nextInt();
+        }
+        CreateFile(acu);
+    }
+
+    /**Verifica si el archivo ya existe para escribir en el*/
+    private void CreateFile(String contenido)
+    {
+        try {
+            File file = new File("Almacen.txt"); /**Creación del archivo*/
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            /**Escritura del archivo*/
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(contenido);
+            bw.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**Lee el archivo y regresa una lista de Comparable*/
+    public Comparable[] lectura()
+    {
+        Comparable[] caja = new Comparable[3000];
+        try
+        {
+            /**Asignación de lineas a array de Comparable*/
+
+            File texto = new File("Almacen.txt");
+            Scanner Lector = new Scanner(texto);
+            while (Lector.hasNext())
+            {
+                int i=0;
+                String datos = Lector.nextLine();
+                caja[i++]=datos;
+
+            }
+
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Archivo no encontrado");
+            e.printStackTrace();
+        }
+        return  caja;
+    }
+
+
+}
